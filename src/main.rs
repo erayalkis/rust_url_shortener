f21b466fe6159b620170d1dbfc230d940babe390
Eray Alkış <erayalkis@hotmail.com>
2023-05-06 20:00:37 +0300

    Create base function for posts/new

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+#[post("/posts/new")]
+async fn create_post(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
