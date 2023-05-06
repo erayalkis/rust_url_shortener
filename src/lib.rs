eb24f0bb6d0379d1b1f30afc23a03bca1ca48925
Eray Alkış <erayalkis@hotmail.com>
2023-05-06 19:49:48 +0300

    Create `create_url_entry` function

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,18 @@
+use diesel::{PgConnection, RunQueryDsl};
+use models::Url;
+use crate::models::NewUrl;
+
 pub mod models;
 pub mod schema;
+
+pub fn create_url_entry(conn: &mut PgConnection, shortened_url: &str, redirect_to: &str) -> Url {
+  use crate::schema::urls;
+
+  let new_url = NewUrl { shortened_url, redirect_to };
+
+  diesel::insert_into(urls::table)
+          .values(&new_url)
+          // Can run `.execute()` here instead if we don't want the return values
+          .get_result(conn)
+          .expect("Error while saving URL record!")
+}
